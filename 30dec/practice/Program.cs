ad789a15a1929fc1cfb57d24ba3e2bc548d82d0b
Nitesh Dotasara <niteshdotasara@gmail.com>
2025-12-30 15:25:28 +0530

    string and 2d aaray codes added in 30dec

---
+++ 30dec/practice/Program.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+class Program {
+
+ // 1)))
+  static void Main() {
+    // string str = Console.ReadLine();
+    // Console.WriteLine(str);
+
+    // //2)))
+
+    // string[] arr = new string[10];
+
+    // for(int i = 0; i < 10; i++) 
+    // {
+    //   arr[i] = Console.ReadLine();
+    // }
+
+    // for(int i = 0; i < 10; i++) 
+    // {
+    //   Console.WriteLine(arr[i]);
+    // }
+
+    //3))
+
+    // int[,] brr=new int[2,2];
+    // for(int i = 0; i < 2; i++)
+    // {
+    //   for(int j = 0; j < 2; j++)
+    //   {
+    //     brr[i,j]=int.Parse(Console.ReadLine());
+    //   }
+    // }
+    
+    // for(int i = 0; i < 2; i++) {
+    //   for(int j = 0; j < 2; j++)
+    //   {
+    //     Console.Write(brr[i,j] + " ");
+    // }
+    //   Console.WriteLine();
+    // }
+
+    //4)))
+
+    //  int[,] a = new int[2,2];
+    // int[,] b = new int[2,2];
+    // int[,] c = new int[2,2];
+
+    // for(int i = 0; i < 2; i++)
+    //   for(int j = 0; j < 2; j++)
+    //     a[i,j] = int.Parse(Console.ReadLine());
+
+    // for(int i = 0; i < 2; i++)
+    //   for(int j = 0; j < 2; j++)
+    //     b[i,j] = int.Parse(Console.ReadLine());
+    
+    // for(int i = 0; i < 2; i++)
+    // {
+    //   for(int j = 0; j < 2; j++)
+    //   {
+    //     c[i,j]=a[i,j]+b[i,j];
+    //   }
+    // }
+    // for(int i = 0; i < 2; i++)
+    // {
+    //   for(int j = 0; j < 2; j++)
+    //   {
+    //     Console.Write(c[i,j]);
+
+    //   }
+    //   Console.WriteLine();
+    // }
+
+    //5)))
+
+    // int[,] a = new int[2,2];
+    // int[,] b = new int[2,2];
+    // int[,] c = new int[2,2];
+
+    // for(int i = 0; i < 2; i++){
+    //   for(int j = 0; j < 2; j++){
+    //     a[i,j] = int.Parse(Console.ReadLine());
+        //}
+        //}     
+
+    // for(int i = 0; i < 2; i++)
+    //   for(int j = 0; j < 2; j++)
+    //     b[i,j] = int.Parse(Console.ReadLine());
+
+    // for(int i = 0; i < 2; i++)
+    //   for(int j = 0; j < 2; j++)
+    //     for(int k = 0; k < 2; k++)
+    //       c[i,j] += a[i,k] * b[k,j];
+
+    // for(int i = 0; i < 2; i++) {
+    //   for(int j = 0; j < 2; j++)
+    //     Console.Write(c[i,j] + " ");
+    //   Console.WriteLine();
+    // }
+
+
+    //6))
+    // for(int i = 0; i < 2; i++) {
+    //   for(int j = 0; j < 2; j++){
+    //     Console.Write(a[j,i] + " ");
+    //   }
+    //   Console.WriteLine();
+    // }
+
+    //7))
+    // int[,] a=new int[3,3];
+    // for(int i = 0; i < 3; i++)
+    // {
+    //   for(int j = 0; j < 3; j++)
+    //   {
+    //    a[i,j]=int.Parse(Console.ReadLine());
+    //   }
+    // }
+    // bool isdiagonal=true;
+    // for(int i = 0; i < 3; i++)
+    // {
+    //   for(int j = 0; j < 3; j++)
+    //   {
+    //     if(i!=j && a[i, j] != 0)
+    //     {
+    //       isdiagonal=false;
+    //     }
+    //   }
+    // }
+    // Console.WriteLine(isdiagonal);
+
+    //8))
+
+    // string str = Console.ReadLine();
+    // char[] arr = str.ToCharArray();
+    // Array.Reverse(arr);
+    // Console.WriteLine(new string(arr));
+
+    //9))
+    // string str = Console.ReadLine();
+
+    // for(int i = str.Length - 1; i >= 0; i--)
+    //   Console.Write(str[i]);
+
+    //10)))
+    //char ch = char.Parse(Console.ReadLine());
+    // if(ch >= 'a' && ch <= 'z')
+    // ch = Convert.ToChar(ch - 32);
+    // else if(ch >= 'A' && ch <= 'Z')
+    // ch = Convert.ToChar(ch + 32);
+
+
+    // Console.WriteLine(ch);
+
+    //11))
+
+    // string str = Console.ReadLine();
+    // char[] arr = str.ToCharArray();
+
+    // for(int i = 0; i < arr.Length; i++) {
+    //   if(arr[i] >= 'a' && arr[i] <= 'z')
+    //     arr[i] = Convert.ToChar(arr[i] - 32);
+    //   else if(arr[i] >= 'A' && arr[i] <= 'Z')
+    //     arr[i] = Convert.ToChar(arr[i] + 32);
+    // }
+
+    // Console.WriteLine(new string(arr));
+
+    //12))
+    // string a = Console.ReadLine();
+    // string b = Console.ReadLine();
+
+    // if(a.Equals(b))
+    //   Console.WriteLine("equal");
+    // else
+    //   Console.WriteLine("not Equal");
+
+    //13))
+    //  string a = Console.ReadLine();
+    // string b = Console.ReadLine();
+    // bool equal = true;
+
+    // if(a.Length != b.Length)
+    //   equal = false;
+    // else {
+    //   for(int i = 0; i < a.Length; i++) {
+    //     if(a[i] != b[i]) {
+    //       equal = false;
+    //       break;
+    //     }
+    //   }
+    // }
+
+    // Console.WriteLine(equal ? "equal" : "not equal");
+
+    //14))
+    // string str1=Console.ReadLine();
+    // string str2=Console.ReadLine();
+
+    // string result=string.Concat(str1,str2);
+    // Console.WriteLine(result);
+
+    //15)))
+    // string a=Console.ReadLine();
+    // string b=Console.ReadLine();
+
+    // char[] result=new char[a.Length+b.Length];
+    // int k=0;
+    // for(int i = 0; i < a.Length; i++)
+    // {
+    //   result[k]=a[i];
+    //   k++;
+    // }
+    // for(int i = 0; i < b.Length; i++)
+    // {
+    //   result[k]=b[i];
+    //   k++;
+    // }
+    // for(int i = 0; i < result.Length; i++)
+    // {
+    //   Console.Write(result[i]);
+    // }
+
+    //16))
+    // string a=Console.ReadLine();
+    // string result=null;
+    // if (a.Contains("dev"))
+    // {
+    //   result= "lodu";
+    // }
+    // else result= " no lodu found";
+    // Console.WriteLine(result);
+  }
+}
--- 30dec/stringarray/Program.cs
+++ 30dec/stringarray/Program.cs
@@ -3,61 +3,114 @@ class Program
 {
     public static void Main(string[] args)
     {
-        string fname, lname;
+        // string fname, lname;
 
-        fname="Pankaj";
-        lname="Sharma";
-        char []letters={'h','e','l','l','o'};
-        string sarray={"Hello", "World","tutorails","point"};
+        // fname="Pankaj";
+        // lname="Sharma";
+        // char []letters={'h','e','l','l','o'};
+        // string sarray={"Hello", "World","tutorails","point"};
 
-        string fullname=fname+lname;
+        // string fullname=fname+lname;
 
-        Console.WriteLine("First Name: " + fname);
-        Console.WriteLine("Last Name: " + lname);
-        Console.WriteLine("Full Name: " + fullname);
-        string greeting=new string(letters);
-        Console.WriteLine("Greting: (0)", greesting);
+        // Console.WriteLine("First Name: " + fname);
+        // Console.WriteLine("Last Name: " + lname);
+        // Console.WriteLine("Full Name: " + fullname);
+        // string greeting=new string(letters);
+        // Console.WriteLine("Greting: (0)", greesting);
 
-        string message=String.Join(" ", sarray);
-        Console.WriteLine("Message: " + message);
+        // string message=String.Join(" ", sarray);
+        // Console.WriteLine("Message: " + message);
         
-        DateTime waiting =new DateTime(2012,10,10,17,58,1);
-        string chat =StringFormat("Mesasage sen at {0:t} on {0:D}",waiting);
-        Console.WriteLine("Message :{0}",chat);
+        // DateTime waiting =new DateTime(2012,10,10,17,58,1);
+        // string chat =StringFormat("Mesasage sen at {0:t} on {0:D}",waiting);
+        // Console.WriteLine("Message :{0}",chat);
 
-        string str1="this is test";
-        string str2="this is test";
-        if (string.Compare(str1, str2) == 0)
+        // string str1="this is test";
+        // string str2="this is test";
+        // if (string.Compare(str1, str2) == 0)
+        // {
+        //     Console.WriteLine(str1+" and "+str2+" are equal");
+        // }
+        // else
+        // {
+        //     Console.WriteLine(str1+" and "+str2+" are not equal");
+        // }
+
+        // string str="this is test";
+        // if (str.Contains("test"))
+        // {
+        //     Console.WriteLine("this sequence contains test");
+        // }
+
+        // string str3="last christmans i gave you my heart";
+        // Console.WriteLine("Before REplace: "+str3);
+        // string substr=str3.Substring(23);
+        // Console.WriteLine(substr);
+
+        // Console.WriteLine("Hello\nWorld");
+        // Console.WriteLine("\"hello world\"");
+        // Console.WriteLine("D:\\projets\\csharp\\program.cs");
+        // Console.WriteLine(@"D:\projects\csharp\program.cs");
+
+        // var message1=@"Hello bro,
+        //         whatss up
+        //         your files have veen ssaved to this daymnn location 
+        //         \shared\blah
+        //         Thanks.";
+        // Console.WriteLine(message1);
+        
+
+        // var name="yolo";
+        // var greet="have a  bad day";
+        // var greeting=String.Format($"HEllo {name}.{greet}");
+
+
+        // var num=12;
+
+        // Console.WriteLine(greeting);
+        // Console.WriteLine($"square of {num}= {num*num}");
+
+
+        // var text="hello world";
+        // var subtet=text.Substring(6,5);
+
+        // Console.WriteLine(subtet);
+        // Console.WriteLine(text.ToUpper());
+
+        // Console.WriteLine(text.ToLower());
+
+        // var text5="   Hello world f ";
+        // Console.WriteLine(text5.Trim());
+
+        // var csv="alicd,bob,chris,dave,eric,fred";
+
+        // var names=csv.Split(',');
+        // for(var i = 0; i < names.Length; i++)
+        // {
+        //     Console.WriteLine(names[i]);
+        // }
+        // var greeeet="helllo boysss letsss go";
+        // var index=greeeet.IndexOf("el");
+        // var index4=greeeet.LastIndexOf("el");
+        // Console.WriteLine(index4);
+
+        int[][] jaggerarr=new int[4][];
+
+        jaggedarr[0]=new int[] {1,2,3,4};
+        jaggedarr[1]=new int[] {5,6,7,8};
+        jaggedarr[2]=new int[] {23,56};
+        jaggedarr[3]=new int[] {7,56,12,0,78,56,33};
+
+        for(int n = 0; n < jaggerarr.Length; n++)
         {
-            Console.WriteLine(str1+" and "+str2+" are equal");
+          System.Console.Write("Row{0}",n); 
+
+            for(int k = 0; k < jaggerarr.Length; k++)
+            {
+                System.Console.Write("{0}",jaggerarr[n][k]);
+            }
         }
-        else
-        {
-            Console.WriteLine(str1+" and "+str2+" are not equal");
-        }
-
-        string str="this is test";
-        if (str.Contains("test"))
-        {
-            Console.WriteLine("this sequence contains test");
-        }
-
-        string str3="last christmans i gave you my heart";
-        Console.WriteLine("Before REplace: "+str3);
-        string substr=str3.Substring(23);
-        Console.WriteLine(substr);
-
-        Console.WriteLine("Hello\nWorld");
-        Console.WriteLine("\"hello world\"");
-        Console.WriteLine("D:\\projets\\csharp\\program.cs");
-        Console.WriteLine(@"D:\projects\csharp\program.cs");
-
-        var message1=@"Hello bro,
-                whatss up
-                your files have veen ssaved to this daymnn location 
-                \shared\blah
-                Thanks.";
-        Console.WriteLine(message1);
+        System.Console.WriteLine();
 
         Console.ReadKey();
 
