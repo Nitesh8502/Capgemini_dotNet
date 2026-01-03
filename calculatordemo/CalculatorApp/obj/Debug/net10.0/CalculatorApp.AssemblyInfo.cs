54a7e0447ce200f97f561442f728553955e0cd28
Nitesh Dotasara <niteshdotasara@gmail.com>
2026-01-03 10:31:07 +0530

    collections added

--- calculatordemo/CalculatorApp/obj/Debug/net10.0/CalculatorApp.AssemblyInfo.cs
+++ calculatordemo/CalculatorApp/obj/Debug/net10.0/CalculatorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad789a15a1929fc1cfb57d24ba3e2bc548d82d0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa76292c49e9e973cd442e0c4f34f80d4beeca04")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
