a21c1d85eb686ba3ec79d3f4c8189846c3ac1130
Nitesh Dotasara <niteshdotasara@gmail.com>
2026-01-06 11:09:35 +0530

    fiels added of indexer

---
+++ 3jan/tasks/assignments/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+class Program
+{
+  static void Main()
+  {
+    
+    Console.Write("enter number of products: ");
+    int n = int.Parse(Console.ReadLine());
+
+    
+    int[] prices = new int[n];
+
+   
+    for (int i = 0; i < prices.Length; i++)
+    {
+      while (true)
+      {
+        Console.Write($"enter price for product {i + 1}: ");
+        int value = int.Parse(Console.ReadLine());
+
+        if (value > 0)
+        {
+          prices[i] = value;
+          break;
+        }
+        else
+        {
+          Console.WriteLine("Price must be positive. Try again.");
+        }
+      }
+    }
+
+
+    int sum = 0;
+    for (int i = 0; i < prices.Length; i++)
+    {
+      sum += prices[i];
+    }
+    int average = sum / prices.Length;
+
+    Array.Sort(prices);
+
+    for (int i = 0; i < prices.Length; i++)
+    {
+      if (prices[i] < average)
+      {
+        prices[i] = 0;
+      }
+    }
+
+    int oldLength = prices.Length;
+    Array.Resize(ref prices, prices.Length + 5);
+
+
+    for (int i = oldLength; i < prices.Length; i++)
+    {
+      prices[i] = average;
+    }
+
+   
+    Console.WriteLine("\nFinal Array Output:");
+    for (int i = 0; i < prices.Length; i++)
+    {
+      Console.WriteLine($"Index {i} : {prices[i]}");
+    }
+  }
+}
--- calculatordemo/CalculatorApp/obj/Debug/net10.0/CalculatorApp.AssemblyInfo.cs
+++ calculatordemo/CalculatorApp/obj/Debug/net10.0/CalculatorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3762602c31b6f1cc9111df68752bb85bc8a1536e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3e2ff3e3fbfae11170eae98a19d298ba980f286")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
