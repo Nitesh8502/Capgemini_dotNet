0bc31f8a44334d386cc97f9a6a6063639e03933f
Nitesh Dotasara <niteshdotasara@gmail.com>
2026-01-06 11:05:29 +0530

    indexer added

---
+++ 6jan/indexer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+  public static void Main()
+  {
+    Employee employee=new Employee();
+    employee[0]="String 1";
+    employee[1]="String 2";
+    employee[2]="String 3";
+    employee[3]="String 4";
+    employee[4]="String 5";
+
+    for(int i = 0; i < 5; i++)
+    {
+      Console.WriteLine(employee[i]);
+    }
+  }
+}
